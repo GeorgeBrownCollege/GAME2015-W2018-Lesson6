d9310f6a46fc124f1873b52af9f07627c5e28c0d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-03-09 16:42:57 -0500

    initial commit

---
+++ Assets/_Scripts/EnemyBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AnimState {
+	IDLE = 0,
+	WALK
+}
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	// reference the Enemy Animator
+	public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Horizontal") != 0) {
+			this.animator.SetInteger ("AnimState", (int)AnimState.WALK);
+		} else {
+			this.animator.SetInteger ("AnimState", (int)AnimState.IDLE);
+		}
+	}
+}
